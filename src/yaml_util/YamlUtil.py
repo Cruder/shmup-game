20d2f17d4151fa5e94b205fb258aeb0370583e29
Caroline Chaudey <caroline.chaudey.sio@gmail.com>
2017-06-25 17:41:46 +0200

    added all_yaml2dic

--- src/yaml_util/YamlUtil.py
+++ src/yaml_util/YamlUtil.py
@@ -8,3 +8,12 @@ def yaml2dic(yaml_path):
             return config
     except FileNotFoundError:
         print("Couldn't parse yaml file ", yaml_path)
+
+
+def all_yaml2dic(yaml_path):
+    try:
+        with open(yaml_path, 'r') as stream:
+            config = yaml.load_all(stream)
+            return config
+    except FileNotFoundError:
+        print("Couldn't parse yaml file ", yaml_path)
