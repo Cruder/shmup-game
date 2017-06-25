7e45a2e19c8a78a95e7ca6d285a0eb014fb1e823
Caroline Chaudey <caroline.chaudey.sio@gmail.com>
2017-06-25 17:48:15 +0200

    refactor

--- src/yaml_util/YamlUtil.py
+++ src/yaml_util/YamlUtil.py
@@ -2,18 +2,16 @@ import yaml
 
 
 def yaml2dic(yaml_path):
-    try:
-        with open(yaml_path, 'r') as stream:
-            config = yaml.load(stream)
-            return config
-    except FileNotFoundError:
-        print("Couldn't parse yaml file ", yaml_path)
+    return yaml.load(yaml_gen(yaml_path))
 
 
 def all_yaml2dic(yaml_path):
+    return yaml.load_all(yaml_gen(yaml_path))
+
+
+def yaml_gen(path):
     try:
-        with open(yaml_path, 'r') as stream:
-            config = yaml.load_all(stream)
-            return config
+        with open(path, 'r') as stream:
+            return stream
     except FileNotFoundError:
-        print("Couldn't parse yaml file ", yaml_path)
+        print("Couldn't parse yaml file ", path)
